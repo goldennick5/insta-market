44831743744013e5dfc7b03ffe2fe1f97abbcde0
goldennick5 <toxaaubakirovv@gmail.com>
2022-10-24 20:00:28 +0600

    Input refactoring

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import Input from "./components/UI/Input/Input";
 function App() {
   return (
     <div className="App">
-      <Input label__focus="Пароль" label="Введите пароль"></Input>
-      <Input label__focus="Пароль" label="Введите пароль"></Input>
-      <Input label__focus="Пароль" label="Введите пароль"></Input>
+
     </div>
   );
 }
