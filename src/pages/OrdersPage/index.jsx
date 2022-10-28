7fc639a5fb1c607eed514d797922f4dfa991d4ac
Makoni1 <67749844+Makoni1@users.noreply.github.com>
2022-10-28 14:23:06 +0600

    order-item

--- src/pages/OrdersPage/index.jsx
+++ src/pages/OrdersPage/index.jsx
@@ -1,4 +1,7 @@
 import o from './OrdersPage.module.scss';
+import orderImg from '../../assets/images/order-img.jpg';
+import arrivedIcon from '../../assets/images/arrivedIcon.svg';
+import arrowDown from '../../assets/images/arrowDown.svg';
 const OrdersPage = () => {
   return (
     <div className={o.order}>
@@ -12,6 +15,30 @@ const OrdersPage = () => {
           <div className={o.status__Btn2}>
             <div className={o.status__text}>Завершенные</div>
             <div className={o.status__number2}>00</div>
+          </div>
+        </div>
+      </div>
+
+      <div className={o.order__item}>
+        <div className={o.item__description}>
+          <div className={o.item__img}>
+            <img src={orderImg} alt="" />
+          </div>
+          <div className={o.item__content}>
+            <div className={o.item__name}>Красные розы</div>
+            <div className={o.item_details}>
+              Заказ #654881 | 10:16 08.06.2022
+            </div>
+          </div>
+        </div>
+        <div className={o.item__description}>
+          <div className={o.item__delivery}>Прибыло в сортировочный центр</div>
+          <div className={o.item__deliveryIcon}>
+            <img src={arrivedIcon} alt="" />
+          </div>
+          <div className={o.item__price}>9 000 ₸</div>
+          <div className={o.item__iconNext}>
+            <img src={arrowDown} alt="" />
           </div>
         </div>
       </div>
