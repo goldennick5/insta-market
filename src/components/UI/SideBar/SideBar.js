d0f003c9b310b400a2f0f13b271577504ab08e03
Makoni1 <67749844+Makoni1@users.noreply.github.com>
2022-11-15 16:28:17 +0600

    sidebar active

--- src/components/UI/SideBar/SideBar.js
+++ src/components/UI/SideBar/SideBar.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import {useNavigate} from "react-router-dom";
-import './SideBar.scss';
+import { useNavigate } from 'react-router-dom';
 import Address from '../../../assets/images/address.svg';
 import Cards from '../../../assets/images/cards.svg';
 import Frame from '../../../assets/images/frame.svg';
@@ -9,61 +8,110 @@ import FAQ from '../../../assets/images/faq.svg';
 import image from '../../../assets/images/ellipse.svg';
 import shopBg from '../../../assets/images/shop-bg.svg';
 import edit from '../../../assets/images/edit.svg';
-
+import { useState } from 'react';
+import s from './SideBar.module.scss';
 function SideBar(props) {
-    const navigate = useNavigate();
-    return (
-        <div className="sidebar">
-            <button className="edit">
-                <img src={edit} alt="edit"/>
-            </button>
-            <div className="sidebar__profile">
-                <div
-                    className="profile__image"
-                    style={{backgroundImage: `url(${image})`}}
-                >
-                    <img src={Frame} alt=""/>
-                </div>
-                <div className="profile__name">{props.signUpName}</div>
-            </div>
-            <div className="services">
-                <div className="services-content" onClick={() => navigate('cabinet/orders')}>
-                    <div className="services__icon">
-                        <img src={Orders} alt="icon"/>
-                    </div>
-                    <div className="services__text">Мои заказы</div>
-                </div>
-                <div className="services-content" onClick={() => navigate('cabinet/cards')}>
-                    <div className="services__icon">
-                        <img src={Cards} alt=""/>
-                    </div>
-                    <div className="services__text">Мои карты</div>
-                </div>
-                <div className="services-content" onClick={() => navigate('cabinet/addresses')}>
-                    <div className="services__icon">
-                        <img src={Address} alt="addresss"/>
-                    </div>
-                    <div className="services__text">Адреса доставки</div>
-                </div>
-            </div>
-            <div className="shop__questions__container">
-                <div className="shop" style={{backgroundImage: `url(${shopBg})`}}>
-                    <div className="shop__text">Хотите открыть свой магазин у нас?</div>
-                </div>
-                <div className="questions">
-                    <div className="questions__content">
-                        <div className="questions__icon">
-                            <img src={FAQ} alt="faq"></img>
-                        </div>
-                        <div className="questions__description">
-                            <div className="questions__text">У Вас есть вопросы?</div>
-                            <div className="questions__subtext">Мы с радостью поможем Вам!</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  const [category, setCategory] = useState('orders');
+  const navigate = useNavigate();
+
+  const nav = (path) => {
+    navigate(`cabinet/${path}`);
+    setCategory(path);
+  };
+
+  return (
+    <div className={s.sidebar}>
+      <button className={s.edit}>
+        <img src={edit} alt="edit" />
+      </button>
+      <div className={s.sidebar__profile}>
+        <div
+          className={s.profile__image}
+          style={{ backgroundImage: `url(${image})` }}
+        >
+          <img src={Frame} alt="" />
         </div>
-    );
+        <div className={s.profile__name}>{props.signUpName}</div>
+      </div>
+      <div className={s.services}>
+        <div
+          className={
+            category === 'orders'
+              ? `${s.services__content} ${s.active}`
+              : s.services__content
+          }
+          onClick={() => nav('orders')}
+        >
+          <div className={s.services__icon}>
+            <img src={Orders} alt="icon" />
+          </div>
+          <div
+            className={
+              s.services__text + (category === 'orders' ? 'active' : '')
+            }
+          >
+            Мои заказы
+          </div>
+        </div>
+        <div
+          className={
+            category === 'cards'
+              ? `${s.services__content} ${s.active}`
+              : s.services__content
+          }
+          onClick={() => nav('cards')}
+        >
+          <div className={s.services__icon}>
+            <img src={Cards} alt="" />
+          </div>
+          <div
+            className={
+              s.services__text + (category === 'cards' ? 'active' : '')
+            }
+          >
+            Мои карты
+          </div>
+        </div>
+        <div
+          className={
+            category === 'addresses'
+              ? `${s.services__content} ${s.active}`
+              : s.services__content
+          }
+          onClick={() => nav('addresses')}
+        >
+          <div className={s.services__icon}>
+            <img src={Address} alt="address" />
+          </div>
+          <div
+            className={
+              s.services__text + (category === 'addresses' ? 'active' : '')
+            }
+          >
+            Адреса доставки
+          </div>
+        </div>
+      </div>
+      <div className={s.shop__questions__container}>
+        <div className={s.shop} style={{ backgroundImage: `url(${shopBg})` }}>
+          <div className={s.shop__text}>Хотите открыть свой магазин у нас?</div>
+        </div>
+        <div className={s.questions}>
+          <div className={s.questions__content}>
+            <div className={s.questions__icon}>
+              <img src={FAQ} alt="faq"></img>
+            </div>
+            <div className={s.questions__description}>
+              <div className={s.questions__text}>У Вас есть вопросы?</div>
+              <div className={s.questions__subtext}>
+                Мы с радостью поможем Вам!
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SideBar;
