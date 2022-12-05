3e74ba3935d6eea0cb6745f1789c3488ac2b1688
goldennick5 <toxaaubakirovv@gmail.com>
2022-12-05 15:48:03 +0600

    .

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import App from './App';
+import store from "./store";
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
 import {Provider} from "react-redux";
-import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
       <Provider store={store}>
