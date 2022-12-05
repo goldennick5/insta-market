c8e6fa95aeea4e0f759373556acdab36bb017546
goldennick5 <toxaaubakirovv@gmail.com>
2022-12-05 16:55:43 +0600

    .

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import Wrapper from "../../components/UI/Wrapper/Wrapper";
 import s from "./SignUp.module.scss";
 import Step1 from "./steps/Step1";
